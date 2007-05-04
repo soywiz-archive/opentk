0fe151662a70be91fef8eccdb88b60bf98967d88
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2007-05-04 12:07:18 +0000

    Moved Glx.cs and Wgl.cs to their respective platforms.
    Moved GLContext to its own project.
    
    Moved DisplayMode.cs and ColorDepth.cs to the GLControl project.
    
    Bumped version info to 0.3.7.0

--- Source/OpenGL/OpenGL/Properties/AssemblyInfo.cs
+++ Source/OpenGL/OpenGL/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OpenGL")]
-[assembly: AssemblyCopyright("Copyright ©  2006 Stefanos Apostolopoulos")]
+[assembly: AssemblyCopyright("Copyright ©  2006, 2007 Stefanos Apostolopoulos")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.3.6.2")]
-[assembly: AssemblyFileVersion("0.3.6.2")]
+[assembly: AssemblyVersion("0.3.7.0")]
+[assembly: AssemblyFileVersion("0.3.7.0")]
