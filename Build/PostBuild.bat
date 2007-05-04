4eebd5b49978e924020818bf0d42f85ab9e5de2c
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2007-05-04 13:42:25 +0000

    Exe files that are not Examples are now output to the "Binaries\Exe" directory.

--- Build/PostBuild.bat
+++ Build/PostBuild.bat
@@ -4,7 +4,7 @@ rem Copying files to output directory
 
 dir Source /s /b  | find ".exe" | find /V "build" | find /V "Examples" > exe.txt
 dir Source /s /b  | find ".dll" | find /V "build" | find /V "Examples" > dll.txt
-for /F "delims='" %%f in (exe.txt) do xcopy "%%f" "Binaries\Libraries\*.*" /Q /Y
+for /F "delims='" %%f in (exe.txt) do xcopy "%%f" "Binaries\Exe\*.*" /Q /Y
 for /F "delims='" %%f in (dll.txt) do xcopy "%%f" "Binaries\Libraries\*.*" /Q /Y
 
 del exe.txt
