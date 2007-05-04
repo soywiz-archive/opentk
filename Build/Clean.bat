728617b3cf9d80bbb8aa420150a6878aeb8d3333
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2007-05-04 12:42:21 +0000

    Updated Clean.bat to clean "obj" directories.
    Renamed PrepareRelease.bat to PrepareSourceRelease.bat

--- Build/Clean.bat
+++ Build/Clean.bat
@@ -1,4 +1,17 @@
-cd ..
-Build\Prebuild /clean /yes /removedir obj /file Prebuild.xml
+echo off
+
+cd..
+
+Build\Prebuild /clean /yes /file Prebuild.xml
+
+echo Cleaning intermediate object files.
+dir /s /b /ad | find "obj" > del.txt
+for /F "delims='" %%f in (del.txt) do rmdir /q /s "%%f"
+del del.txt
+
+echo Cleaning binaries.
 rd Binaries /S /Q
-pause
+
+cd Build
+
+echo on
--- Build/PrepareSourceRelease.bat
+++ Build/PrepareSourceRelease.bat
@@ -1,5 +1,10 @@
 cd..
+
+Build\Clean.bat
+
+echo Cleaning .svn directories.
 dir /s /b /ad | find ".svn" > del.txt
 for /F "delims='" %%f in (del.txt) do rmdir /q /s "%%f"
 del del.txt
+
 cd Build
