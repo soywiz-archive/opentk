5301a542894e4ac17b563aa10e574163fc4c4cf8
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-12-10 23:14:20 +0000

    Branch for OpenCL support.

---
+++ Source/OpenTK/Compute/Structs.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenTK.Compute
+{
+    public struct DeviceId
+    {
+        IntPtr handle;
+    }
+
+    public struct Context
+    {
+        IntPtr handle;
+    }
+
+    public struct CommandQueue
+    {
+        IntPtr handle;
+    }
+
+    public struct Memory
+    {
+        IntPtr handle;
+    }
+
+    public struct Program
+    {
+        IntPtr handle;
+    }
+
+    public struct Kernel
+    {
+        IntPtr handle;
+    }
+
+    public struct Event
+    {
+        IntPtr handle;
+    }
+
+    public struct Sampler
+    {
+        IntPtr handle;
+    }
+}
