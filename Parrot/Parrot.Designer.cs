7e3f488f1094cdf18a5bf6f5ddaa5ba18143cc30
Chrisbrandtner <Chrisbrandtner@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2009-07-04 13:54:09 +0000

    2 standalone examples. Will only build with openal-next branch.

---
+++ Parrot/Parrot.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Parrot
+{
+    partial class Parrot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox_RecorderSelection = new System.Windows.Forms.ComboBox();
+            this.groupBox_RecorderParameters = new System.Windows.Forms.GroupBox();
+            this.label_MONO16BIT = new System.Windows.Forms.Label();
+            this.label_RINGBUFFER = new System.Windows.Forms.Label();
+            this.label_FREQUENCY = new System.Windows.Forms.Label();
+            this.numericUpDown_SamplesRingbuffer = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown_Frequency = new System.Windows.Forms.NumericUpDown();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label_SamplesConsumed = new System.Windows.Forms.Label();
+            this.button_Stop = new System.Windows.Forms.Button();
+            this.button_Start = new System.Windows.Forms.Button();
+            this.timer_GetSamples = new System.Windows.Forms.Timer(this.components);
+            this.groupBox_RecorderParameters.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_SamplesRingbuffer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Frequency)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox_RecorderSelection
+            // 
+            this.comboBox_RecorderSelection.FormattingEnabled = true;
+            this.comboBox_RecorderSelection.Location = new System.Drawing.Point(6, 19);
+            this.comboBox_RecorderSelection.Name = "comboBox_RecorderSelection";
+            this.comboBox_RecorderSelection.Size = new System.Drawing.Size(229, 21);
+            this.comboBox_RecorderSelection.TabIndex = 0;
+            // 
+            // groupBox_RecorderParameters
+            // 
+            this.groupBox_RecorderParameters.Controls.Add(this.label_MONO16BIT);
+            this.groupBox_RecorderParameters.Controls.Add(this.label_RINGBUFFER);
+            this.groupBox_RecorderParameters.Controls.Add(this.label_FREQUENCY);
+            this.groupBox_RecorderParameters.Controls.Add(this.numericUpDown_SamplesRingbuffer);
+            this.groupBox_RecorderParameters.Controls.Add(this.numericUpDown_Frequency);
+            this.groupBox_RecorderParameters.Controls.Add(this.comboBox_RecorderSelection);
+            this.groupBox_RecorderParameters.Location = new System.Drawing.Point(12, 12);
+            this.groupBox_RecorderParameters.Name = "groupBox_RecorderParameters";
+            this.groupBox_RecorderParameters.Size = new System.Drawing.Size(245, 103);
+            this.groupBox_RecorderParameters.TabIndex = 2;
+            this.groupBox_RecorderParameters.TabStop = false;
+            this.groupBox_RecorderParameters.Text = "Recording Device Setup";
+            // 
+            // label_MONO16BIT
+            // 
+            this.label_MONO16BIT.AutoSize = true;
+            this.label_MONO16BIT.Location = new System.Drawing.Point(176, 50);
+            this.label_MONO16BIT.Name = "label_MONO16BIT";
+            this.label_MONO16BIT.Size = new System.Drawing.Size(64, 13);
+            this.label_MONO16BIT.TabIndex = 102;
+            this.label_MONO16BIT.Text = "Mono 16 Bit";
+            // 
+            // label_RINGBUFFER
+            // 
+            this.label_RINGBUFFER.AutoSize = true;
+            this.label_RINGBUFFER.Location = new System.Drawing.Point(6, 76);
+            this.label_RINGBUFFER.Name = "label_RINGBUFFER";
+            this.label_RINGBUFFER.Size = new System.Drawing.Size(158, 13);
+            this.label_RINGBUFFER.TabIndex = 101;
+            this.label_RINGBUFFER.Text = "Samples in AL internal ringbuffer";
+            // 
+            // label_FREQUENCY
+            // 
+            this.label_FREQUENCY.AutoSize = true;
+            this.label_FREQUENCY.Location = new System.Drawing.Point(6, 50);
+            this.label_FREQUENCY.Name = "label_FREQUENCY";
+            this.label_FREQUENCY.Size = new System.Drawing.Size(84, 13);
+            this.label_FREQUENCY.TabIndex = 100;
+            this.label_FREQUENCY.Text = "Frequency in Hz";
+            // 
+            // numericUpDown_SamplesRingbuffer
+            // 
+            this.numericUpDown_SamplesRingbuffer.Location = new System.Drawing.Point(165, 72);
+            this.numericUpDown_SamplesRingbuffer.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown_SamplesRingbuffer.Minimum = new decimal(new int[] {
+            1024,
+            0,
+            0,
+            0});
+            this.numericUpDown_SamplesRingbuffer.Name = "numericUpDown_SamplesRingbuffer";
+            this.numericUpDown_SamplesRingbuffer.Size = new System.Drawing.Size(70, 20);
+            this.numericUpDown_SamplesRingbuffer.TabIndex = 2;
+            this.numericUpDown_SamplesRingbuffer.ThousandsSeparator = true;
+            this.numericUpDown_SamplesRingbuffer.Value = new decimal(new int[] {
+            16000,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown_Frequency
+            // 
+            this.numericUpDown_Frequency.Location = new System.Drawing.Point(94, 46);
+            this.numericUpDown_Frequency.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown_Frequency.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown_Frequency.Name = "numericUpDown_Frequency";
+            this.numericUpDown_Frequency.Size = new System.Drawing.Size(70, 20);
+            this.numericUpDown_Frequency.TabIndex = 1;
+            this.numericUpDown_Frequency.ThousandsSeparator = true;
+            this.numericUpDown_Frequency.Value = new decimal(new int[] {
+            16000,
+            0,
+            0,
+            0});
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label_SamplesConsumed);
+            this.groupBox2.Controls.Add(this.button_Stop);
+            this.groupBox2.Controls.Add(this.button_Start);
+            this.groupBox2.Location = new System.Drawing.Point(12, 121);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(245, 78);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Talk to the parrot";
+            // 
+            // label_SamplesConsumed
+            // 
+            this.label_SamplesConsumed.AutoSize = true;
+            this.label_SamplesConsumed.Location = new System.Drawing.Point(6, 52);
+            this.label_SamplesConsumed.Name = "label_SamplesConsumed";
+            this.label_SamplesConsumed.Size = new System.Drawing.Size(129, 13);
+            this.label_SamplesConsumed.TabIndex = 2;
+            this.label_SamplesConsumed.Text = "Samples consumed: 5432";
+            // 
+            // button_Stop
+            // 
+            this.button_Stop.Location = new System.Drawing.Point(129, 19);
+            this.button_Stop.Name = "button_Stop";
+            this.button_Stop.Size = new System.Drawing.Size(110, 24);
+            this.button_Stop.TabIndex = 1;
+            this.button_Stop.Text = "Stop recording";
+            this.button_Stop.UseVisualStyleBackColor = true;
+            this.button_Stop.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button_Start
+            // 
+            this.button_Start.Location = new System.Drawing.Point(6, 19);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(110, 24);
+            this.button_Start.TabIndex = 0;
+            this.button_Start.Text = "Start recording";
+            this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer_GetSamples
+            // 
+            this.timer_GetSamples.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Parrot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(269, 211);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox_RecorderParameters);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Parrot";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OpenAL Parrot";
+            this.groupBox_RecorderParameters.ResumeLayout(false);
+            this.groupBox_RecorderParameters.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_SamplesRingbuffer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Frequency)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox_RecorderSelection;
+        private System.Windows.Forms.GroupBox groupBox_RecorderParameters;
+        private System.Windows.Forms.Label label_RINGBUFFER;
+        private System.Windows.Forms.Label label_FREQUENCY;
+        private System.Windows.Forms.NumericUpDown numericUpDown_SamplesRingbuffer;
+        private System.Windows.Forms.NumericUpDown numericUpDown_Frequency;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label_SamplesConsumed;
+        private System.Windows.Forms.Button button_Stop;
+        private System.Windows.Forms.Button button_Start;
+        private System.Windows.Forms.Label label_MONO16BIT;
+        private System.Windows.Forms.Timer timer_GetSamples;
+
+    }
+}
+
---
+++ Parrot/Parrot.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using OpenTK.Audio;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Parrot
+{
+    public partial class Parrot : Form
+    {
+        public Parrot()
+        {
+            InitializeComponent();
+
+            this.Text = "OpenAL Parrot (" + (IntPtr.Size == 4 ? "x86" : "x64") + ")";
+
+            #region Fill the combobox
+
+            IList<string> recorders = AudioDeviceEnumerator.AvailableRecordingDevices;
+            for (int i = 0; i < recorders.Count; i++)
+            {
+                if (!String.IsNullOrEmpty(recorders[i]))
+                    comboBox_RecorderSelection.Items.Add(recorders[i]);
+            }
+            comboBox_RecorderSelection.SelectedIndex = 0;
+
+            #endregion Fill the combobox
+        }
+
+        #region Forms related
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button_Start.Enabled = false;
+            button_Stop.Enabled = true;
+
+            groupBox_RecorderParameters.Enabled = false;
+
+            this.StartRecording();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button_Start.Enabled = true;
+            button_Stop.Enabled = false;
+
+            groupBox_RecorderParameters.Enabled = true;
+
+            this.StopRecording();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.GetSamples();
+        }
+
+        #endregion Forms related
+
+        #region OpenAL related
+
+        private AudioContext AC;
+        private int src;
+        private AudioCapture rec;
+        private int Frequency;
+        private IntPtr BufferPtr;
+        private GCHandle BufferHandle;
+        private byte[] BufferArray;
+        private const byte SampleToByte = 2;
+
+        private void StartRecording()
+        {
+            try
+            {
+                AC = new AudioContext();
+            }
+            catch (AudioException ae)
+            {
+                MessageBox.Show("Exception caught when opening playback device.\n" + ae.Message);
+            }
+            if (AC == null)
+                return;
+
+            AL.Listener(ALListenerf.Gain, 64.0f);
+            src = AL.GenSource();
+
+            try
+            {
+                MessageBox.Show("Using recorder:" +
+                                "\nName: " + (string)comboBox_RecorderSelection.SelectedItem +
+                                "\nFrequency: " + (uint)numericUpDown_Frequency.Value +
+                                "\nSamples: " + (int)numericUpDown_SamplesRingbuffer.Value);
+                rec = new AudioCapture((string)comboBox_RecorderSelection.SelectedItem, (uint)numericUpDown_Frequency.Value, ALFormat.Mono16, (int)numericUpDown_SamplesRingbuffer.Value);
+            }
+            catch (AudioDeviceException ade)
+            {
+                MessageBox.Show("Exception caught when opening recording device.\n" + ade.Message);
+                rec = null;
+            }
+
+            if (rec == null)
+                return;
+
+            Frequency = (int)numericUpDown_Frequency.Value;
+
+            BufferArray = new byte[(int)numericUpDown_SamplesRingbuffer.Value * SampleToByte];
+            BufferHandle = GCHandle.Alloc(BufferArray, GCHandleType.Pinned);
+            BufferPtr = BufferHandle.AddrOfPinnedObject();
+
+            rec.Start();
+
+            timer_GetSamples.Start();
+            timer_GetSamples.Interval = 1000 / 20; // tick every 50ms
+        }
+
+        private void StopRecording()
+        {
+            if (rec == null)
+                return;
+
+            timer_GetSamples.Stop();
+
+            rec.Stop();
+            rec.Dispose();
+            rec = null;
+
+            int r;
+            AL.GetSource(src, ALGetSourcei.BuffersQueued, out r);
+            ClearBuffers(r);
+
+            AL.DeleteSource(src);
+
+            Frequency = 1;
+
+            BufferHandle.Free();
+            BufferPtr = IntPtr.Zero;
+            BufferArray = null;
+        }
+
+        private void GetSamples()
+        {
+            if (rec == null)
+                return;
+
+            int SamplesBefore = rec.AvailableSamples;
+            if (SamplesBefore == 0)
+                return;
+
+            rec.GetSamples(BufferPtr, SamplesBefore);
+            int SamplesConsumed = SamplesBefore - rec.AvailableSamples;
+            label_SamplesConsumed.Text = "Samples consumed: " + SamplesConsumed;
+
+            int buf = AL.GenBuffer();
+            AL.BufferData(buf, ALFormat.Mono16, BufferPtr, (int)(SamplesConsumed * SampleToByte), Frequency);
+            AL.SourceQueueBuffer(src, buf);
+            if (AL.GetSourceState(src) != ALSourceState.Playing)
+                AL.SourcePlay(src);
+
+            ClearBuffers(0);
+        }
+
+        public void ClearBuffers(int input)
+        {
+            int[] freedbuffers;
+            if (input == 0)
+            {
+                int BuffersProcessed;
+                AL.GetSource(src, ALGetSourcei.BuffersProcessed, out BuffersProcessed);
+                if (BuffersProcessed == 0)
+                    return;
+                freedbuffers = AL.SourceUnqueueBuffers(src, BuffersProcessed);
+            }
+            else
+            {
+                freedbuffers = AL.SourceUnqueueBuffers(src, input);
+            }
+            AL.DeleteBuffers(freedbuffers);
+        }
+
+        #endregion OpenAL related
+
+    }
+}
