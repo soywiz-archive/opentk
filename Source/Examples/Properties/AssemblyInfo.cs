3e89f99da55c93f526d0f19b89e2feaeedce0dd6
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2010-03-24 15:26:13 +0000

    Updated documentation and version numbers for 1.0rc1 release.

--- Source/Examples/Properties/AssemblyInfo.cs
+++ Source/Examples/Properties/AssemblyInfo.cs
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.103")]
-[assembly: AssemblyFileVersion("1.0.0.103")]
+[assembly: AssemblyVersion("1.0.0.201")]
+[assembly: AssemblyFileVersion("1.0.0.201")]
 
 #if SIGN_ASSEMBLY
 [assembly: AssemblyKeyFile(@"../../../OpenTK.snk")]
